63c8591c4852be86c84b633e5eea25b46a2458f6
Wellington Amaral <well.ribeiro@live.com>
2022-09-19 11:45:13 -0300

    Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import { ThemeProvider } from '@mui/material';
 import theme from 'ui/themes/theme';
 import React from 'react';
+import Header from 'ui/components/surfaces/Header/Header';
+import Footer from 'ui/components/surfaces/Footer/Footer';
+import { AppContainer } from '@styles/pages/_app.styled';
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+    <Head>
+      <title>E-diaristas {pageProps.title && ` - ${pageProps.title}`}</title>
+    </Head>
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+     <AppContainer>
+            <Header />
+            <main>
+              <Component {...pageProps} />
+            </main>
+            <Footer/>
+      </AppContainer>
     </ThemeProvider>
       
     </>
